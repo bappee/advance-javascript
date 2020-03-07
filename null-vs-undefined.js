09ab35510a4b781786cb8a18071ea321546ff31a
bappee <robiullislambappy@gmail.com>
2020-03-07 10:36:06 +0600

    null vs undefined done

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return
+
+// }
+// const result= add(20,25);
+// console.log(result);
+
+
+// const premik= { name:"mahi", phone:0125};
+// console.log(premik.num);
+
+
+let roll = [z,25,65];
+console.log(roll[0]);
---
+++ true-false.js
@@ -0,0 +1,25 @@
+const age=4;
+if(age){
+    console.log( "age is true");
+}
+else{
+    console.log("age is false");
+}
+
+
+
+// falsy value
+//0
+//""
+//null
+//NaN
+//undefined
+
+let num=NaN;
+if(num){
+    console.log( "age is true");
+}
+else{
+    console.log("age is false");
+}
+
